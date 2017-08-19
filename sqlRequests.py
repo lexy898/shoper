622bbc7a1b51ed38d56d7cdab2935f16130ab06b
AKozitsin <kaknikak898@gmail.com>
2017-08-20 01:35:26 +0300

    Полность сделано меню телеграмм-бота

--- sqlRequests.py
+++ sqlRequests.py
@@ -119,10 +119,171 @@ def getBrands():
     try:
         conn = sqlite3.connect(_DB_PATH)
         cursor = conn.cursor()
-        cursor.execute("SELECT company_name FROM company")
+        cursor.execute("SELECT id, company_name FROM company")
         brands = cursor.fetchall()
-        result = [x[0] for x in brands]
+        result = {}
+        for i in range(len(brands)):
+            result[brands[i][0]] = brands[i][1]
         conn.close()
         return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getSubscriptions(chatId): #получить названия брендов, на которые подписан пользователь
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT c.company_name FROM company c "
+                       "INNER JOIN compilance_user_prod cup ON cup.company = c.id "
+                       "WHERE cup.chatid = '"+str(chatId)+"'")
+        subscriptions = cursor.fetchall()
+        result = [x[0] for x in subscriptions]
+        conn.close()
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getTypesOfGood():
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, description FROM type_of_goods")
+        typesOfGood = cursor.fetchall()
+        result = {}
+        for i in range(len(typesOfGood)):
+            result[typesOfGood[i][0]] = typesOfGood[i][1]
+        conn.close()
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getCurrentCompanyByUser(chatId):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT company FROM compilance_user_prod "
+                       "WHERE chatid = '"+str(chatId)+"' AND flag = 1")
+        currentCompany = cursor.fetchall()
+        result = currentCompany[0][0]
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getTypesOfGoodByCompany(company):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT type_of_good FROM compilance_comp_prod "
+                       "WHERE company_id = (SELECT id FROM company "
+                       "WHERE company_name = '"+str(company)+"')")
+        typesOfGood = cursor.fetchall()
+        result = [x[0] for x in typesOfGood]
+        conn.close()
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getTypesOfGoodByUser(chatid, company):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT type_of_good FROM compilance_user_prod "
+                       "WHERE company = (SELECT id FROM company "
+                       "WHERE company_name = '"+str(company)+"') "
+                       "AND chatid = '"+str(chatid)+"'")
+        typesOfGood = cursor.fetchall()
+        result = [x[0] for x in typesOfGood]
+        conn.close()
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def addSubscribeBrand(chatid, brand):
+    typesOfGood = getTypesOfGoodByCompany(brand)
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id from company WHERE company_name = '"+brand+"'")
+        id = cursor.fetchall()
+        id = id[0][0]
+        for i in range(len(typesOfGood)):
+            cursor.execute("INSERT INTO compilance_user_prod (chatid, type_of_good, company) "
+                           "VALUES ('"+str(chatid)+"',"+str(typesOfGood[i])+", "+str(id)+")")
+        conn.commit()
+        conn.close()
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def delSubscribeBrand(chatid, brand):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id from company WHERE company_name = '"+brand+"'")
+        id = cursor.fetchall()
+        id = id[0][0]
+        cursor.execute("DELETE FROM compilance_user_prod WHERE chatid = '"+str(chatid)+"' AND company = "+str(id))
+        conn.commit()
+        conn.close()
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def addSubscribeTypeOfGoods(chatid, typeOfGood):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO compilance_user_prod (chatid, type_of_good, company) "
+                       "VALUES ("+str(chatid)+", (SELECT id FROM type_of_goods "
+                                             "WHERE description = '"+str(typeOfGood)+"'),"
+                       "(SELECT company FROM compilance_user_prod WHERE "
+                       "chatid = "+str(chatid)+" AND flag = 1))")
+        conn.commit()
+        conn.close()
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def delSubscribeTypeOfGoods(chatid, typeOfGood):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM  compilance_user_prod WHERE "
+                       "chatid = '"+str(chatid)+"' AND type_of_good = "
+                                           "(SELECT id FROM type_of_goods "
+                                             "WHERE description = '"+str(typeOfGood)+"')")
+        conn.commit()
+        conn.close()
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def getUsersBrands(chatid): #Получить id всех брендов, на которые подписан пользователь
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT company FROM compilance_user_prod "
+                       "WHERE chatid = '"+str(chatid)+"'")
+        usersBrands = cursor.fetchall()
+        result = [x[0] for x in usersBrands]
+        conn.close()
+        return result
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def resetFlagByUser(chatid):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("UPDATE compilance_user_prod SET flag = null WHERE chatid = '"+str(chatid)+"'")
+        conn.commit()
+        conn.close()
+    except sqlite3.DatabaseError as err:
+        logging.error(u'' + str(err) + '')
+
+def setFlagByUser(chatid, company):
+    try:
+        conn = sqlite3.connect(_DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute("UPDATE compilance_user_prod SET flag = 1 WHERE chatid = '"+str(chatid)+"' "
+                        "AND company = (SELECT id FROM company WHERE company_name = '"+company+"')")
+        conn.commit()
+        conn.close()
     except sqlite3.DatabaseError as err:
         logging.error(u'' + str(err) + '')
--- telegramBot.py
+++ telegramBot.py
@@ -10,36 +10,115 @@ token = "426617203:AAHqKOH-62wRF1XSEFR8NS6372nHMWNR0BE"
 chat_id = 158041048
 
 bot = telebot.TeleBot(token)
+brands = sqlRequests.getBrands()
+typesOfgood = sqlRequests.getTypesOfGood()
+
+def setHomeScreen(message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Мои подписки', 'Бренды')
+    markup.row('🚫')
+    bot.send_message(message.chat.id, "🖖", reply_markup=markup)
+
+def setBrandSettingsScreen(message):
+    typesOfGoodByCompany = sqlRequests.getTypesOfGoodByCompany(message.text)
+    typesOfGoodByUser = sqlRequests.getTypesOfGoodByUser(message.chat.id, message.text)
+    subscribe = list(set(typesOfGoodByCompany).difference(typesOfGoodByUser))
+    markup = types.ReplyKeyboardMarkup()
+    for i in range(len(subscribe)):
+        markup.row("✅Подписаться на " + typesOfgood[subscribe[i]])
+    for i in range(len(typesOfGoodByUser)):
+        markup.row("❌Отписаться от " + typesOfgood[typesOfGoodByUser[i]])
+    markup.row("🏠")
+    bot.send_message(message.chat.id, "Choose:", reply_markup=markup)
+
+def refreshBrandSettingsScreen(message):
+    try:
+        currentCompany = sqlRequests.getCurrentCompanyByUser(message.chat.id)
+        typesOfGoodByCompany = sqlRequests.getTypesOfGoodByCompany(brands[currentCompany])
+        typesOfGoodByUser = sqlRequests.getTypesOfGoodByUser(message.chat.id, brands[currentCompany])
+        subscribe = list(set(typesOfGoodByCompany).difference(typesOfGoodByUser))
+        markup = types.ReplyKeyboardMarkup()
+        for i in range(len(subscribe)):
+            markup.row("✅Подписаться на " + typesOfgood[subscribe[i]])
+        for i in range(len(typesOfGoodByUser)):
+            markup.row("❌Отписаться от " + typesOfgood[typesOfGoodByUser[i]])
+        markup.row("🏠")
+        bot.send_message(message.chat.id, "Choose:", reply_markup=markup)
+    except:
+        setHomeScreen(message)
+
+def setBrandsScreen(message):
+    usersBrands = sqlRequests.getUsersBrands(message.chat.id)
+    listBrands = list(brands.keys())
+    subscribe = list(set(listBrands).difference(usersBrands))
+    markup = types.ReplyKeyboardMarkup()
+    for i in range(len(subscribe)):
+        markup.row("✅Подписаться на " + brands[subscribe[i]])
+    for i in range(len(usersBrands)):
+        markup.row("❌Отписаться от " + brands[usersBrands[i]])
+    markup.row("🏠")
+    bot.send_message(message.chat.id, "Choose one brand:", reply_markup=markup)
+
+def setMySubscriptionsScreen(message):
+    sqlRequests.resetFlagByUser(message.chat.id)  # сброс флага текущего бренда у пользователя
+    subscriptions = sqlRequests.getSubscriptions(message.chat.id)
+    markup = types.ReplyKeyboardMarkup()
+    for i in range(len(subscriptions)):
+        markup.row(subscriptions[i])
+    markup.row("🏠")
+    bot.send_message(message.chat.id, "Your subscriptions:", reply_markup=markup)
+
+def subscribe(message):
+    key = message.text[16:]
+    if key in brands.values():
+        sqlRequests.addSubscribeBrand(message.chat.id, key)
+        bot.send_message(message.chat.id, "Подписка на " + key + " оформлена!\n "
+                                                                 "Настроить подписку можно в разделе \"Мои подписки\"")
+        setBrandsScreen(message)
+    elif key in typesOfgood.values():
+        sqlRequests.addSubscribeTypeOfGoods(message.chat.id, key)
+        bot.send_message(message.chat.id, "Подписка на " + key + " добавлена!")
+        refreshBrandSettingsScreen(message)
+
+def unsubscribe(message):
+    key = message.text[15:]
+    if key in brands.values():
+        sqlRequests.delSubscribeBrand(message.chat.id, key)
+        bot.send_message(message.chat.id, "Подписка на " + key + " отменена💔")
+        setBrandsScreen(message)
+    elif key in typesOfgood.values():
+        sqlRequests.delSubscribeTypeOfGoods(message.chat.id, key)
+        bot.send_message(message.chat.id, "Подписка на " + key + " отменена💔")
+        refreshBrandSettingsScreen(message)
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
-    markup = types.ReplyKeyboardMarkup()
-    markup.row('Мои подписки','Бренды')
-    markup.row('🚫')
-    bot.send_message(message.chat.id, "Choose one letter:", reply_markup=markup)
+    setHomeScreen(message)
 
-    @bot.message_handler(content_types=['text'])
-    def handle_text(message):
-        if message.text == "Мои подписки":
-            bot.send_message(message.chat.id, "Твои подписки")
-        elif message.text == "Бренды":
-            brands = sqlRequests.getBrands()
-            markup = types.ReplyKeyboardMarkup()
-            for i in range(len(brands)):
-                print(brands[i])
-                markup.row(brands[i])
-            bot.send_message(message.chat.id, "Choose one brand:", reply_markup=markup)
-
-        elif message.text == "🚫":
-            markdown = types.ReplyKeyboardRemove(selective=False)
-            bot.send_message(message.chat.id, "Пока 😜", reply_markup=markdown)
-        else:
-            bot.send_message(message.chat.id, "Не понимаю, что ты мне хочешь сказать")
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text == "Мои подписки":
+        setMySubscriptionsScreen(message)
+    elif message.text == "Бренды":
+        setBrandsScreen(message)
+    elif list(brands.values()).count(message.text) != 0: #если сообщение равно значению одного из брендов
+        sqlRequests.resetFlagByUser(message.chat.id)
+        sqlRequests.setFlagByUser(message.chat.id, message.text)
+        setBrandSettingsScreen(message)
+    elif "Подписаться на" in message.text:
+        subscribe(message)
+    elif "Отписаться от" in message.text:
+        unsubscribe(message)
+    elif message.text == "🏠":
+        setHomeScreen(message)
+    elif message.text == "🚫":
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, "Пока 😜", reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, "Не понимаю, что ты мне хочешь сказать")
 
 @bot.message_handler(commands=['chatID'])
 def handle_start_help(message):
     bot.send_message(message.chat.id, message.chat.id)
 
 bot.polling(none_stop=True, interval=0)
-
-def view_brand_menu(chatid):
