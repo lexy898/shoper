d64b1aae35cd77a4c11f4960e29023ee48aea862
AKozitsin <kaknikak898@gmail.com>
2017-09-24 00:48:12 +0300

    Испралена отправка оъявлений

--- send_ADS.py
+++ send_ADS.py
@@ -6,6 +6,9 @@ TOKEN = "426617203:AAHqKOH-62wRF1XSEFR8NS6372nHMWNR0BE"
 bot = telebot.TeleBot(TOKEN)
 allSubscribers = sql_requests.get_all_subscribers()
 print(allSubscribers)
-TEXT = '🌈Появился новый магазин ROXY!\n Спеши подписаться на самые свежие товары со скидкой!\n Официальный интернет-магазин в России: http://www.roxy-russia.ru/'
+TEXT = '🌈Появился новый магазин Adidas!\n Спеши подписаться на самые свежие товары со скидкой!\n Официальный интернет-магазин в России: http://www.adidas.ru/'
 for subscriber in allSubscribers:
-    bot.send_message(str(subscriber), TEXT)
+    try:
+        bot.send_message(str(subscriber), TEXT)
+    except:
+        continue
